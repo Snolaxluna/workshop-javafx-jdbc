a32f60e88b5ff49438815eb88956b7b06fe66172
Snolaxluna <lucassolanich1987@gmail.com>
2023-09-23 14:59:16 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 
 public class Main extends Application {
 
+    private static Scene mainScene;
     @Override
     public void start(Stage stage) {
         try {
@@ -19,13 +20,17 @@ public class Main extends Application {
             scrollPane.setFitToWidth(true);
             scrollPane.setFitToHeight(true);
 
-            Scene mainScene = new Scene(scrollPane);
+            mainScene = new Scene(scrollPane);
             stage.setScene(mainScene);
             stage.setTitle("Sample JavaFX application");
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static Scene getMainScene(){
+        return mainScene;
     }
 
     public static void main(String[] args) {
